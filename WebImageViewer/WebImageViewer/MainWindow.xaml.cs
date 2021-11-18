2a5c260fc00f62fc7e36b09d8f3278e70649a44b
蘿蔔 <55518831+iambjlu@users.noreply.github.com>
2021-11-18 15:08:26 +0800

    加入專案檔案。

---
+++ WebImageViewer/WebImageViewer/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WebImageViewer
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        BitmapImage myBitmapImage = new BitmapImage();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Go_Click(object sender, RoutedEventArgs e)
+        {
+            //wb.Navigate(new Uri(tb.Text));
+            //<WebBrowser x:Name="wb" Margin="0,41,-0.4,0"/>
+            
+            myBitmapImage.BeginInit();
+            myBitmapImage.UriSource = new Uri(tb.Text);
+            myBitmapImage.EndInit();
+            myImage.Source = myBitmapImage;
+        }
+    }
+}
